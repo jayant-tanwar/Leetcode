10a229604be879faa5b6c8cd8e1d74598bd2f307
JAYANT TANWAR <34964323+vajnir@users.noreply.github.com>
2020-08-06 16:01:17 +0530

    Update Knapsack.java

--- Knapsack.java
+++ Knapsack.java
@@ -94,3 +94,187 @@ Coin Change
         }
         return DP[c.size()][n];
     }
+
+------------------------------------------------------------------------------------------------
+    Last Stone Weight II
+    Problem Statement: We have a collection of rocks, each rock has a positive integer weight.
+        Each turn, we choose any two rocks and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash i
+        If x == y, both stones are totally destroyed;
+        If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+        At the end, there is at most 1 stone left.  Return the smallest possible weight of this stone (the weight is 0 if there are no stones left.)
+            
+            
+            
+    Solution: The final rock would be a summation of all the values with giving one set of values positive sign and
+    the other set of values the negative sign. The goal is to minimise the weight (magnitude) of the final rock. The problem,
+    breaks down into finding two sets (paritioning the stones array) which have the smallest difference.
+    Therefore, this problem can be reformulated into a 0-1 Knapsack problem, where the value (weights) of the rocks
+    is maximised and their total weight (value) does not exceed math.floor(sum of all stones/2).
+        
+     
+    Solution using 2D DP:
+
+        public int lastStoneWeightII{int[] stones){
+            int sum=IntStream.of(stones).sum();
+            //sum holds the total sum of the stones array
+            
+            boolean[][] DP=new boolean[stones.length+1][(sum/2)+1];
+            //rows as stones, columns as sum
+            //DP[i][j] means picking i elements (inclusive) can the sum 'j' be formed?
+            // If yes, then DP[i][j] is true
+            // Otherwise, DP[i][j] is false
+            
+            
+            DP[0][0]=true;
+            //You can form zero sum using zero elements.
+            
+            for(int i=1;i<=stones.length;i++){
+                DP[i][0]=DP[i-1][0];
+                //You can form zero sum NOT USING your ith element
+            }
+            
+            int MaxFound=0;
+            //The actual answer would be sum-(2*MaxFound)
+            //Because let us assume your sum is 23 then you cannot divide it into two perfectly
+            //But you can have a set with sum as 11 or something lower.
+            
+            
+            for(int i = 1 ; i <= stones.length ; i++ ){
+                
+                for(int j=1 ; j<=sum/2 ; j++){
+                    
+                    int value = stones[i-1]; //weight and value are same, i-1 because indexing...
+                    
+                    if(j >= value ){
+                        
+                        DP[i][j]=DP[i-1][j-v];
+                        // if ith element is chosen to be in the set when the max sum is j, then
+                        // to accomodate v in the set, borrow the bool value from DP[i-1][j-v]
+                        
+                        if(DP[i][j]==true){
+                            MaxFound=Math.max(MaxFound,j);
+                            //Update the max if sum j can be reached.
+                        }
+                        DP[i][j] | = DP[i-1][j]; 
+                        // If ith element is not chosen to be in the set
+                        
+                        
+                    }
+                }
+            }
+            
+            return sum - (2*MaxFound);
+            
+        }
+                                     
+                                     
+    Solution Using 1D DP version 1: We use two rows of length sum/2
+        public int lastStoneWeightII(int[] stones){
+            int sum=IntStream.of(stones).sum();
+            
+            boolean[] DP=new boolean[1+(sum/2)];
+            //Just one row, this time.
+            
+            DP[0]=true;
+            //It is possible to get sum = '0' using 0 elements.
+            
+            int MaxFound=0;
+            
+            for ( int i = 1 ; i < = stones.length ; i++ ){
+                int value=stones[i-1];
+                
+                boolean[] DP2=new boolean[1+(sum/2)];
+                //Why a second row?
+                //This is a 0-1 Knapsack problem, so every element can
+                //only be collected once. Unlike the coin change problem.
+                
+                for( int j = sum/2  ; j > = 0 ; j -- ){
+                    //But why traverse the opposite way
+                    //Because let us say that at a certain value of j
+                    //DP[j] is true, because the previous i-1 th elements
+                    //were able to build a sum equals to j
+                    //So, now when the ith element is chosen to be and
+                    //calculation for sum J is done, then if j+ value of ith element
+                    //equals J then, DP[J] shall be set to True.
+                    // A loop from j=0 to sum/2 can also work.
+                    
+                    if( j - value > = 0 ){
+                        // i th element can be included in the set
+                        DP2[j]  =  DP[j-value];
+                        //Here j behaves as J and j-v as j
+                        //Because it is a fact that if v is added to j-v then
+                        //output is j and if DP[j-v] was true then DP[j] should be
+                        //true as well.
+                        
+                       
+                    }
+                    
+                    DP2[j] | = DP[j-value];
+                    if(DP2[j]){
+                            MaxFound=Math.max(MaxFound,j);
+                             //This if condition can actuall come before the above line.
+                     }
+                    //If ith element cannot be included or if you choose not to do so.
+                }
+                DP=DP2;
+                // Updating the old DP by the new DP
+                // Now the old DP has the calculations till ith element (inclusive)
+            }
+        
+        return sum - (2*MaxFound) ;
+    }
+                                     
+                    
+        
+    Solution Using 1D DP version 2: We use a single row of length sum/2
+        
+        public int lastStoneWeightII(int[] stones){
+            
+            int sum=IntStream.of(stones).sum();
+            
+            DP[0]=true;
+            int MaxFound=0;
+            
+        
+             for(int i = 1; i < = stones.length ; i ++){
+                   
+                 int v=stones[i-1];
+                 
+                 for(int j = sum/2 ; j>=v ; j-- ){
+                     
+                     //In this case you cannot run a loop from j = 0 to sum/2
+                     //You can but then it will be complicated
+                     
+                     if( j - v >=0 ){
+                         DP[j]|=DP[j-v]; 
+                         //If DP[j] is already true and if DP[j-v] is false then do not
+                         //make DP[j] false
+                     }
+                     
+                     if(DP[j]){
+                         MaxFound = Math.max( MaxFound, j );
+                     }
+                 }
+             }
+        return sum - (2*MaxFound);
+        
+    }
+                                     
+                     
+                     
+
+                               
+                                     
+                  
+                                     
+                                     
+                  
+                     
+                             
+                            
+                    
+            
+            
+            
+            
+            
