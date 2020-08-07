01f995601eb423d00e6778538db7a1dec73393e4
JAYANT TANWAR <34964323+vajnir@users.noreply.github.com>
2020-08-07 15:29:23 +0530

    Update Q11 Longest Substring with At Least K Repeating Characters.java

--- Q11 Longest Substring with At Least K Repeating Characters.java
+++ Q11 Longest Substring with At Least K Repeating Characters.java
@@ -1,5 +1,4 @@
 /*
-This is kinda hard man.
 Longest Substring with At Least K Repeating Characters
 Find the length of the longest substring T of a given string (consists of lowercase letters only) such that every character in T appears no less than k times.
 
@@ -60,7 +59,69 @@ class Solution{
         }
        
           
-          
+   
+  Solution 2: A Better Solution, it is recursive, but is it divide and conquer?
+    In every time of DC, at least 1 character, let us say 'a', is chosen to divide the string,
+    then all substrings in following recursive calls do not have the character 'a'. Therefore,
+  the number of levels are limited to at most 26, because after that you run out of characters.
+    Each level is O(N). 
+    (On leetcode this beats 100% 0-ms solution)
+  
+    class Solution {
+    public int longestSubstring(String s, int k) {
+        char[] strn=s.toCharArray();
+        return longestSubstring(strn,0,s.length()-1,k);      
+      }
+   
+
+    private int longestSubstring(char[] strn,int start, int end, int frequency){
+        int N=end-start+1;
+        int s=start;
+        int e=end;
+        int k=frequency;
+    
+        if(s==e && k==1) return 1;
+        if(N<k) return 0;
+        
+        int[] FrequencyArray=new int[26];
+        for(int i=s;i<=e;i++){
+            char ch=strn[i];
+            int index=ch-'a';
+            FrequencyArray[index]+=1;
+        }
+        
+        boolean Condition_Satisfied=true;
+        
+        for(int i=s;i<=e;i++){
+            char ch=strn[i];
+            int index=ch-'a';
+            if(FrequencyArray[index]<k){
+                Condition_Satisfied=false;
+            }
+        }
+        
+        if(Condition_Satisfied==true){
+            return N;
+        }
+        
+        int Res=0;
+        int i=s;
+        int j=s;
+        
+        while(j<=e){
+            char ch=strn[j];
+            int f=FrequencyArray[ch-'a'];
+            if(f<k){
+                Res=Math.max(Res,longestSubstring(strn,i,j-1,k));
+                i=j+1;
+            }
+            j=j+1;
+        }
+        
+        return Math.max(Res,longestSubstring(strn,i,e,k));
+
+    }
+}
           
           
           
