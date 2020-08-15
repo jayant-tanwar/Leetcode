a219826effce5734229e80829146b3fc84753c90
JAYANT TANWAR <34964323+vajnir@users.noreply.github.com>
2020-08-15 15:45:53 +0530

    Update KthSmallestElementinSortedMatrix.java

--- KthSmallestElementinSortedMatrix.java
+++ KthSmallestElementinSortedMatrix.java
@@ -9,11 +9,74 @@ Example:
 and k = 8 
 Answer 13
 
+Solution: Using Binary Search
+Time Complexity: (Please confirm this) O( N*log(max-min) ) ///log(max-min) component comes from the range binary search over N*N elements and N component comes from the two loops
+Space Complexity: O(1) //Big advantage!
 
-    public int kthSmallest(int[][] matrix, int k){
-        int n = matrix.length;
-        int m = matrix[0].length;
+class Solution{
+	public int KthSmallest(int[][] Matrix,int k){
+		//Using Binary Search
+		int N=Matrix.length;
+		int s=Matrix[0][0];
+		int e=Matrix[N-1][N-1];
+
+		while(s<e){
+			int mid=(s+e)>>1;
+
+			int j=N-1;
+			int count=0;
+
+			for(int i=0;i<N;i++){
+				while(j>=0 && Matrix[i][j]>mid){
+					j--;
+				}
+				count=count+(j+1);
+			}
+
+			if(count<k){
+				s=mid+1;
+			}
+			else{
+				e=mid;
+			}
+
+
+		}
+		return e;
+
+	}
+}
+
+
+Solution: Using Priority Queue!
+Time Complexity: O(min(K,N) + K*log(N)) //Please check this! I think it should be K*log(min(K,N))
+Space Complexity: O(N)
+
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int N=matrix.length;
+		PriorityQueue<int[]> PQ=new PriorityQueue<int[]>((a,b)->(matrix[a[0]][a[1]] - matrix[b[0]][b[1]]));
+        //mini comparator?
         
-        int s = matrix[0][0];
-        int e = matrix[n-1][m-1];
+        //Only min(N,k) elements in the Queue will be there at any time.
+		for(int i=0;i<Math.min(N,k);i++){
+			PQ.add(new int[]{0,i});
+		}
 
+		while(k!=0){
+			int[] A=PQ.poll();
+            k--;
+			if(k==0){
+				return matrix[A[0]][A[1]];
+			}
+			else if(A[0]<N-1){ //check to see if you are going out of the matrix
+                
+                //Since my right element already exists in the PQ
+                // I will add the element bigger than current element which is the one below
+                
+				PQ.add(new int[]{A[0]+1,A[1]});
+			}
+		}
+		return -1;
+	}
+}
