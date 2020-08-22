2fe8eacf8e860780e6671211603d99b270dd5fbb
JAYANT TANWAR <34964323+vajnir@users.noreply.github.com>
2020-08-22 12:35:13 +0530

    Update Linkedlists.java

--- Linkedlists.java
+++ Linkedlists.java
@@ -1,1 +1,39 @@
+Questions in this document:
+1. Reverse LinkedList
+2. Reverse LinkedList II
+3. Reorder LinkedList
 
+
+1. Reverse LinkedList
+Solution 1: Iteratively
+
+public Listnode reverseList(ListNode head){
+    ListNode CurNode = head;
+    ListNode PrevNode = null;
+    
+    while(CurNode!=null){
+      ListNode NextNode = CurNode.next;
+      CurNode.next = PrevNode;
+      PrevNode = CurNode;
+      
+      CurNode = NextNode;
+   }
+   
+   return PrevNode;  
+}
+
+Solution 2: Recursive
+
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null){
+            return head;
+        }
+        
+        ListNode A = reverseList(head.next);
+        
+        head.next.next=head;
+        head.next=null;
+        return A;
+
+        
+    }
